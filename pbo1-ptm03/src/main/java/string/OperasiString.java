e92de69457d4f56d34921a986fce503254e61f8b
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 11:29:23 +0800

    Update OperasiString.java
    Menambahkan Println

--- pbo1-ptm03/src/main/java/string/OperasiString.java
+++ pbo1-ptm03/src/main/java/string/OperasiString.java
@@ -10,6 +10,8 @@ package string;
  */
 public class OperasiString {
     public static void main(String[] args) {
+        String kota = "Yogyakarta";
         
+        System.out.println(kota);
     }
 }

8cc30372b7c196344c1f0e129fd1a29f5d89765a
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 11:56:49 +0800

    Update Switch.java
    perubahan switch

--- pbo1-ptm03/src/main/java/percabangan/Switch.java
+++ pbo1-ptm03/src/main/java/percabangan/Switch.java
@@ -9,6 +9,7 @@ package percabangan;
  * @author Welcomp
  */
 public class Switch {
+    /*
     public static void main(String[] args) {
         int jalanJalan = 3;
         switch(jalanJalan){
@@ -26,6 +27,33 @@ public class Switch {
                 break;
             default:
                 System.out.println("mahluk halus");
+                break;
+        }
+    }
+*/
+    
+    public static void main(String[] args) {
+        char nilai = 'D';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mhs   : \"Terimakasih pak\"");
+                System.out.println("Dosen : \"Selamat ya!\"");
+                break;
+            case 'B':
+                System.out.println("Mhs   : \"Kenapa ulun kada dapat A pak?\"");
+                System.out.println("Dosen : \"@#$%&*!\"");
+                break;
+            case 'C':
+                System.out.println("Mhs   : \"Ulun turun pul pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"Tapi bisalah menjawab ujian?\"");
+                System.out.println("Mhs   : \"Hihihihi\"");
+                break;
+            default:
+                System.out.println("Mhs   : \"Ulun turun pul pak ai, tugas pul jua\"");
+                System.out.println("Dosen : \"Bujur jua kah?\"");
+                System.out.println("Dosen : \"Memeriksa berkas...\"");
+                System.out.println("Dosen : \"Menceleng\"");
+                System.out.println("Mhs   : \"Kabur...\"");
                 break;
         }
     }

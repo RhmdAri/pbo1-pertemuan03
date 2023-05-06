d7d0fb0e348cf63d2a566254138902b3bc66b725
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 11:41:21 +0800

    Create OperatorUnary.java
    Operator Unary

---
+++ pbo1-ptm03/src/main/java/operator/OperatorUnary.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;                        //i tadinya 0 ditambahkan 1 sehingga i = 1
+        System.out.println(i);    //betulkan i nilainya 1
+        i++;                        //i tadinya 1 ditambahkan 1 lagi menjadi 2
+        System.out.println(i);    //tuh betulkan i nilainya 2
+        i=i+1;                      //bisa dilihat i++ sama dengan i=i+1
+        System.out.println(i);    //i yang tadinya 2 sekarang menjadi 3
+    }
+}

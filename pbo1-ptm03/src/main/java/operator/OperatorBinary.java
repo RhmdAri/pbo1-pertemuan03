50c65c417376faf4d6be631958fad52b141039cc
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 11:37:05 +0800

    Create OperatorBinary.java
    Operator Binary

---
+++ pbo1-ptm03/src/main/java/operator/OperatorBinary.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operator;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+   //menempatkan nilai ke dalam variable
+   //baris 13-15 adalah comment/komentar yang tidak dijalankan oleh program
+    
+   String nama = "Rahmad Ari Wijaya";
+   int jumlahSks = 24;
+   double indeksPrestasiKumulatif = 3.89;
+}

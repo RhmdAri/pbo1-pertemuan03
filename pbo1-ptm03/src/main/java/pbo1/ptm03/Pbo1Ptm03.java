638515a2a6736908d4ca149225e15da0ea715d79
RhmdAri <rhmdari1907@gmail.com>
2023-05-06 11:23:00 +0800

    Create Pbo1Ptm03.java

---
+++ pbo1-ptm03/src/main/java/pbo1/ptm03/Pbo1Ptm03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pbo1.ptm03;
+
+/**
+ *
+ * @author Welcomp
+ */
+public class Pbo1Ptm03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
